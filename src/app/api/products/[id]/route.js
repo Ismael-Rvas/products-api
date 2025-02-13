96a971b2d8f74004447029d9f4ce7d8487c123e6
Ismael-Rvas <ismael.rivas.developer@gmail.com>
2025-02-13 21:45:48 +0100

    Retocar api.rest

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,5 +1,5 @@
 import { connectToDatabase } from "@/lib/mongodb";
-import { ObjectId } from "mongodb"; 
+import { ObjectId } from "mongodb";
 
 export async function GET(request, { params }) {
     const { database } = await connectToDatabase();
@@ -22,8 +22,8 @@ export async function PUT(request, { params }) {
     const collection = database.collection(process.env.MONGODB_COLLECTION);
 
     const { id } = await params
-    const {  name, price, img, inventoryDate } = await request.json() // Read body request
-    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: {  name, price, img, inventoryDate } });
+    const { name, price, img, inventoryDate } = await request.json() // Read body request
+    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { name, price, img, inventoryDate } });
 
     return Response.json(results);
 }
